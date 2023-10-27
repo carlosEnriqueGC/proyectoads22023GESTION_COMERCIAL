92a5b19571af4cf61aed2816d992893fd4c14ae8
carlos enrique <cguzmanc10@miumg.edu.gt>
2023-10-27 16:06:10 -0600

    clases conexion sentencias y controlador agregadas

--- Codigo/Modulos/Administracion/ComprasCxp/Capa_vista/Properties/Resources.Designer.cs
+++ Codigo/Modulos/Administracion/ComprasCxp/Capa_vista/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Vista_PrototipoMenu.Properties {
+namespace CapaVistaComercial.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Vista_PrototipoMenu.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Vista_PrototipoMenu.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CapaVistaComercial.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
---
+++ Codigo/Modulos/Administracion/ComprasCxp/Modelo/Sentencias.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
+
+namespace CapaModeloComercial
+{
+    class Sentencias
+    {
+        Conexion con = new Conexion();
+
+    }
+}
